7a7b5370d484e735da9be160957e78e11eb354bc
kinocode <kinocode@kinocode-no-MacBook-Pro.local>
2018-06-01 23:43:43 +0900

    a

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-Vue.config.productionTip = false
-
-Vue.component('start', {
-  template: '#start'
-})
-
-Vue.component('next', {
-  template: '#next'
-})
-
-new Vue({
-  el: '#app',
-  
-  data: {
-    page: 'start'
-  },
-  
-  methods: {
-    onChangePage(to) {
-      this.page = to
-    }
-  }
-})
+const vm = new Vue({
+    el: "#qa", 
+    data: {
+      q1: null,
+      q2: null,
+      q3: null,
+      a: "えのき"
+  }})
